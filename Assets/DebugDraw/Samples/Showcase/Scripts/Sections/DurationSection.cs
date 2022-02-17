c1ddaa37bee893d746b1bdb40f06b721bd9bf83e
C <thecmann1@gmail.com>
2022-02-17 16:33:45 +0200

    Add duration section

---
+++ Assets/DebugDraw/Samples/Showcase/Scripts/Sections/DurationSection.cs
@@ -0,0 +1,33 @@
+using System;
+using UnityEngine;
+
+namespace DebugDrawSamples.Showcase.Sections
+{
+
+	[RequireComponent(typeof(Icon))]
+	public class DurationSection : BaseSection
+	{
+
+		public float duration = 0.5f;
+		public float length = 1;
+		public float spawnGap = 0.1f;
+		
+		private float t;
+		
+		private void Update()
+		{
+			t += Time.deltaTime;
+
+			if (t > spawnGap)
+			{
+				t -= spawnGap;
+				
+				Vector3 p = tr.position;
+				Vector3 u = tr.up * length * 0.5f;
+				DebugDraw.Line(p - u, p + u, Showcase.NiceColor(), duration);
+			}
+		}
+
+	}
+
+}
